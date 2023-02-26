d33e62996d6c24c39a26be16ca969161e8827fd2
Mahi <mahipasarkar@gmail.com>
2023-02-26 13:10:19 -0500

    Initial Commit

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node
+{
+    public enum Status {SUCCESS, RUNNING, FAILURE};
+    public Status status;
+    public List<Node> children = new List<Node>();
+    public int currentChild = 0;
+    
+
+}
